0e5d6a321f4db783b2e8abb492f0931f0fef9310
Elton Andrade <eltonsa75@hotmail.com>
2020-03-13 00:01:24 -0300

    Validando Formulario

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -13,6 +13,13 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = ''
   public formaPagamento: string = ''
 
+  //controle de validação dos campos
+  public enderecoValido: boolean
+  public numeroValido: boolean
+  public complementoValido: boolean
+  public formaPagamentoValido: boolean
+
+
   constructor() { }
 
   ngOnInit() {
@@ -20,22 +27,42 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco
-    console.log(this.endereco)
+    //console.log(this.endereco)
+    if(this.endereco.length > 3) {
+      this.enderecoValido = true
+    } else {
+      this.enderecoValido = false
+    }
+
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero
-    console.log(this.numero)
+    //console.log(this.numero)
+    if(this.numero.length > 0) {
+      this.numeroValido = true
+    } else {
+      this.numeroValido = false
+    }
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento
-    console.log(this.complemento)
+    //console.log(this.complemento)
+    if(this.complemento.length > 0) {
+      this.complementoValido = true
+    } 
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento
-    console.log(this.formaPagamento)
+    //console.log(this.formaPagamento)
+
+    if (this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true
+    } else {
+      this.formaPagamentoValido = false
+    }
   }
 
 }
