8a84a7f7457e722b836b01d145f2c5e569dd1258
Elton Andrade <elton.andrade@globalhitss.com.br>
2020-03-27 14:33:56 -0300

    Validação de Formulário

---
+++ src/app/ordem-compra.service.ts
@@ -0,0 +1,27 @@
+
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { Pedido } from './shared/pedido.model';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { URL_API } from './app.api';
+
+@Injectable()
+export class OrdemCompraSerrvice {
+
+    constructor(private http: HttpClient){}
+
+    public efetivarCompra(pedido : Pedido): Observable<any> {
+
+       // Requisição em Post
+        let headers: HttpHeaders = new HttpHeaders()
+        headers.append('Content-type', 'application/json')
+
+        return this.http.post(
+            `${URL_API}/pedidos`,
+            (pedido),
+            ({headers: headers})
+        )
+        .map((resposta: Response) => console.log(resposta))
+    }
+}
--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,13 +1,21 @@
+import { Pedido } from './../shared/pedido.model';
+import { OrdemCompraSerrvice } from './../ordem-compra.service';
 import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css']
+  styleUrls: ['./ordem-compra.component.css'],
+  providers: [ OrdemCompraSerrvice]
 })
 export class OrdemCompraComponent implements OnInit {
 
-  // Atributos do temlate
+  //Pedido
+  public pedido: Pedido = new Pedido('', '', '', '' )
+  
+  // Atributos do template
   public endereco: string = ''
   public numero: string = ''
   public complemento: string = ''
@@ -20,49 +28,98 @@ export class OrdemCompraComponent implements OnInit {
   public formaPagamentoValido: boolean
 
 
-  constructor() { }
+  //Estados primitivos dos campos (pristine)
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
+
+
+//controlar botão confirmar compra
+public formEstado: string = 'disabled'
+
+  constructor(private ordemCompraService: OrdemCompraSerrvice) { }
 
   ngOnInit() {
+    // this.ordemCompraService.efetivarCompra()
   }
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco
     //console.log(this.endereco)
+
+    this.enderecoEstadoPrimitivo = false
+
     if(this.endereco.length > 3) {
       this.enderecoValido = true
     } else {
       this.enderecoValido = false
     }
 
+    this.habilitaForm()
+
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero
     //console.log(this.numero)
+
+    this.numeroEstadoPrimitivo = false
+
     if(this.numero.length > 0) {
       this.numeroValido = true
     } else {
       this.numeroValido = false
     }
+
+    this.habilitaForm()
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento
     //console.log(this.complemento)
+
+    this.complementoEstadoPrimitivo = false
+
     if(this.complemento.length > 0) {
       this.complementoValido = true
     } 
+
+    this.habilitaForm()
   }
 
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento
     //console.log(this.formaPagamento)
 
+    this.formaPagamentoEstadoPrimitivo = false
+
     if (this.formaPagamento.length > 0) {
       this.formaPagamentoValido = true
     } else {
       this.formaPagamentoValido = false
     }
+
+    this.habilitaForm()
+  }
+
+  public habilitaForm(): void {
+    if(this.enderecoValido === true && this.numeroValido === true &&  this.formaPagamentoValido === true) {
+      this.formEstado = ''
+    } else {
+      this.formEstado = 'disabled'
+    }
+  }
+
+  public confirmarCompra(): void {
+
+    this.pedido.endereco = this.endereco
+    this.pedido.numero = this.numero
+    this.pedido.complemento = this.complemento
+    this.pedido.formaPagamento = this.formaPagamento
+
+    this.ordemCompraService.efetivarCompra(this.pedido)
+    .subscribe()
   }
 
 }
